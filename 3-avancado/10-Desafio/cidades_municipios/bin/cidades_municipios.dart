f6ff27ef516c76af6179975ba2e033e5f86b92cf
Igor Barreto <igorbarreto1993@hotmail.com>
2020-10-14 22:08:57 -0300

    Criando o projeto cidades para o desafio avançado

---
+++ 3-avancado/10-Desafio/cidades_municipios/bin/cidades_municipios.dart
@@ -0,0 +1,6 @@
+import 'package:cidades_municipios/cidades_municipios.dart'
+    as cidades_municipios;
+
+void main(List<String> arguments) {
+  cidades_municipios.run();
+}
---
+++ 3-avancado/10-Desafio/cidades_municipios/test/cidades_municipios_test.dart
@@ -0,0 +1,8 @@
+import 'package:cidades_municipios/cidades_municipios.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
